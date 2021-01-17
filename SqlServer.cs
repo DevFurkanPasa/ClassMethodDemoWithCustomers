7a00203020e7b2fb32757e0c51e518da63647b7e
DevFurkanTopal <furkan.topal8118@gmail.com>
2021-01-17 19:05:36 +0300

    SqlServer updating

--- SqlServer.cs
+++ SqlServer.cs
@@ -10,9 +10,9 @@ namespace ClassMethodDemo
         {
             Customer customer1 = new Customer();
             customer1.Id = 0;
-            customer1.FirstName = "Murat";
-            customer1.LastName = "TOPAL";
-            customer1.Age = 56;
+            customer1.FirstName = "Engin";
+            customer1.LastName = "Demiroğ";
+            customer1.Age = 36;
             customer1.PhoneNumber = "+905862486598";
             
             
@@ -25,15 +25,15 @@ namespace ClassMethodDemo
             
             Customer customer3 = new Customer();
             customer3.Id = 2;
-            customer3.FirstName = "Fatma";
-            customer3.LastName = "TOPAL";
-            customer3.Age = 54;
+            customer3.FirstName = "Halil İbrahim";
+            customer3.LastName = "Duymaz";
+            customer3.Age = 26;
             customer3.PhoneNumber = "+9051425636987";
 
             Customer customer4 = new Customer();
             customer4.Id = 3;
-            customer4.FirstName = "Feyza";
-            customer4.LastName = "Çetin";
+            customer4.FirstName = "Kerem";
+            customer4.LastName = "Varış";
             customer4.Age = 28;
             customer4.PhoneNumber = "+905483659281";
 
